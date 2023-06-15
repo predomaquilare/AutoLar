1056389b4d5e9806bfada85395dbb2dfab3f6262
Pedro <pedro.santana@academico.ifpb.edu.br>
2023-06-14 21:26:07 -0300

    sf

---
+++ code/test_api_google_assistant.ino
@@ -0,0 +1,20 @@
+#include <Wifi.h>
+
+#define IO_USERNAME  "puredo_"
+#define IO_KEY       "aio_jXVv39N0uLMGZ70se1IRA4aUZW2S"
+
+
+
+#define WLAN_SSID       "...your SSID..."
+#define WLAN_PASS       "...your password..."
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883          
+
+
+void setup() {
+
+}
+
+void loop() {
+
+}
